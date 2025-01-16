2dcd87ce230f071552c8dfc13a561befc6aad9b2
Luc Pacheco <luc.pacheco@outlook.com>
2025-01-16 14:25:31 +0100

    009 - Connect to BDD : add postgresql configuration and maven dependency

--- src/main/java/com/deviived/angularbackofficebackend/config/MvcConfig.java
+++ src/main/java/com/deviived/angularbackofficebackend/config/MvcConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfig implements WebMvcConfigurer {
 
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/api/movies").setViewName("movies");
+//        registry.addViewController("/api/movies").setViewName("movies");
         registry.addViewController("/").setViewName("home");
         registry.addViewController("/login").setViewName("login");
     }
